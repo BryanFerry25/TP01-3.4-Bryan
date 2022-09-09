6d23a5f3f07a1f41e7369c8f6f3ec9f8aa098357
BryanFerry25 <bryan.ferry@edu.univ-fcomte.fr>
2022-09-09 14:29:28 +0200

    affiche du composant maison avec v-bind

---
+++ src/pages/simple.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="p-2">
+        <h1 class="text-2xl">Page Simple</h1>
+
+
+        <card class="w-1/2" v-bind="objet"/>
+    </div>
+    </template>
+    
+    <script setup lang="ts">
+        import card from "../components/card.vue";
+    
+        const objet = {nom:"Maison", favoris:true, price:4000, nbsize:"200 m²", txt:"Joli maison", nbbath:2, img:"/public/maison.jpg"}
+    </script>
