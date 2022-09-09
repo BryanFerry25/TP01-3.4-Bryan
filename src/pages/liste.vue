07ab3580458a3ed87018dd7e8f589796168e9979
BryanFerry25 <bryan.ferry@edu.univ-fcomte.fr>
2022-09-09 15:04:21 +0200

    Ajout de 2 autres maison pour liste

--- src/pages/liste.vue
+++ src/pages/liste.vue
@@ -14,10 +14,10 @@
 </template>
 
 <script setup lang="ts">
-    
+
 import card from "../components/card.vue";
 
-const maisons = [{nom:"Maison", price:5000, favoris:true, txt:"Belle Maison", nbbath:3, nbsize:"500 m²", img:"/public/maison.jpg"}, {nom:"Maison 2", price:4500, favoris:false, txt:"Agréable maison", nbbath:1, nbsize:"300 m²", img:"/public/maison2.jpg"}]
+const maisons = [{nom:"Maison", price:5000, favoris:true, txt:"Belle Maison", nbbath:3, nbsize:"500 m²", img:"/public/maison.jpg"}, {nom:"Maison 2", price:4500, favoris:false, txt:"Agréable maison", nbbath:1, nbsize:"300 m²", img:"/public/maison2.jpg"},{nom:"Maison 3", price:6000, txt:"Magnifique maison", favoris:true, nbbath:5, nbsize:"700 m²", img:"/public/maison3.jpg"},{nom:"Maison 4", price:2000, txt:"Basique maison", nbbath:2, nbsize:"100 m²", img:"/public/maison4.jpg"}]
 
 
 </script>
