687c262a49e10d6d9e14694d774d9b278f30b3ab
BryanFerry25 <bryan.ferry@edu.univ-fcomte.fr>
2022-09-16 13:55:27 +0200

    importation formkit + création page new

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import routesFromPages from "~pages";
 
+import { plugin, defaultConfig } from "@formkit/vue";
+
+
+
 import "./index.css";
 
 const router = createRouter({
@@ -16,4 +20,5 @@ const router = createRouter({
 
 const app = createApp(App);
 app.use(router);
+app.use(plugin, defaultConfig);
 app.mount("#app");
