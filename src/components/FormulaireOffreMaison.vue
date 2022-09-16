f25e56437accf375b12f79997cd7f6a18fbea47d
BryanFerry25 <bryan.ferry@edu.univ-fcomte.fr>
2022-09-16 14:30:49 +0200

    personnalisation

--- src/components/FormulaireOffreMaison.vue
+++ src/components/FormulaireOffreMaison.vue
@@ -17,7 +17,16 @@
         </div>
      <div class="p-2">
         <!-- On passe la "ref" à FormKit -->
-        <FormKit type="form" :submit-attrs="{ classes: { input: 'bg-indigo-300 p-1 rounded' } }" :config="{classes: {input: 'p-1 rounded  border-gray-300 shadow-sm border hover:bg-indigo-100', label: 'text-indigo-800'}}" v-model="maison" >
+        <FormKit type="form"
+        
+        :submit-attrs="{ classes: { input: 'bg-red-300 p-2 rounded' } }" 
+        
+        :config="{classes: {
+            
+        input: 'p-2 rounded  border-black-300 shadow-sm border hover:bg-indigo-300', 
+        label: 'text-indigo-700'}}" v-model="maison" >
+
+
            <FormKit name="nom" label="Nom" />
            <FormKit name="price" label="Prix" type="number" />
            <FormKit name="nbbath" label="Nombre de salle de bain" type="number" />
